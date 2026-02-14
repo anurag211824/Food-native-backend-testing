28aef86a2156b4401a644db5bfb94680b2255045
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2026-02-14 13:26:36 +0530

    add cors middleware and update package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "dotenv/config"
 import express from 'express';
 import type { Request, Response } from 'express';
+import cors from 'cors';
 import router from './routes/index.js';
 import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = Number(process.env.PORT || 3000);
+app.use(cors({ origin: '*' }));
 app.use(cookieParser());
 app.use(express.json());
 
